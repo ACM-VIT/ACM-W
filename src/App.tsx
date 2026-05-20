0f0d3332c672c56983cedf14bb081342db13e3b6
Maitri-shah29 <maitrirajivshah@gmail.com>
2026-05-21 01:05:24 +0530

    dummy blog data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import BlogStamps from "./pages/BlogStamps";
 import WomenInStem from "./pages/WomenInStem";
 
-function App() {
-  return <WomenInStem />;
-}
+export default function App() {
+  return (
+    <main className="overflow-x-hidden">
+      <section
+        style={{
+          position: "relative",
+          zIndex: 20,
+        }}
+      >
+        <BlogStamps />
+      </section>
 
-export default App;
+      <section
+        style={{
+          position: "relative",
+          zIndex: 10,
+          background: "#fff9e9",
+        }}
+      >
+        <WomenInStem />
+      </section>
+    </main>
+  );
+}
